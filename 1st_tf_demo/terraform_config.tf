b25c90e774294ad23ac6f6e56823c021fc74974c
Rati Krishna Moharana <mratikrishna@gmail.com>
2026-03-18 16:32:49 +0530

    initial terraform projects and AWS EC2 created

---
+++ 1st_tf_demo/terraform_config.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.54.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "simple_ec2_instance" {
+  ami           = "ami-02dfbd4ff395f2a1b"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "SimpleEC2Instance"
+  }
+}
